478ce9f2831cae8f8afd466478654ddaf27620e1
singclare <grochi1995@gmail.com>
2019-06-26 11:37:48 +0900

    Create py4e-Ch5Part2.py

---
+++ py4e-Ch5Part2.py
@@ -0,0 +1,15 @@
+##Loops and Iteration##
+##Definite Loops
+# >because they execut and exact number of times
+# >definite loops iterate through the members of a set
+
+##A Simple Definite Loop
+for i in [5, 4, 3, 2, 1] :
+    print(i) # i is iterate variable
+print('Blastoff!')
+
+##A Definite Loop with Strings
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends :
+    print('Hally New Year:', friend)
+print('Done!')
